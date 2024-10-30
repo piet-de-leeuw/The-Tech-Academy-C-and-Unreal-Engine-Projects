73e3a614a10bb652315f1d345a30c4d4f0a036a1
piet-de-leeuw <pietdeleeuw156@gmail.com>
2024-10-30 15:06:55 +0100

    Added handle.h

---
+++ C++/SQLite/sqlite_1/sqlite_1/Handle.h
@@ -0,0 +1,144 @@
+#pragma once
+
+#ifdef _DEBUG
+	#include <crtdbg.h>
+	#define ASSERT _ASSERTE
+#else
+	#define ASSERT __noop
+#endif
+
+template <typename T>
+struct HandleTraits
+{
+	using Type = T;
+
+	// A way to identify invalid handles
+	// Every conrete traits class cann override it shoult thay want to spesify a non null value for invalide handles. 
+	static Type Invalid() noexcept
+	{
+		return nullptr;
+	}
+
+	// We leave it undefined because we expect every concrete class to have one of its own.
+	// static void Close(Type value) noexcept
+};
+
+template <typename Traits> 
+class Handle
+{
+	// using the returntype from Traits::Invalid() to assign it to Type;
+	using Type = decltype(Traits::Invalid());
+	Type m_value;
+
+	// Called in the destructor
+	void Close() noexcept
+	{
+		if (*this)
+		{
+			Traits::Close(m_value); 
+		}
+	}
+
+public:
+	// Prohibit copy semantics
+	Handle(Handle const&) = delete;
+	Handle& operator=(Handle const&) = delete;
+
+
+	// Default constructor
+	explicit Handle(Type value = Traits::Invalid()) noexcept : 
+		m_value(value)
+	{}
+
+	// Move semantics
+	Handle(Handle && other) noexcept :
+		m_value(other.Detach())
+	{}
+
+	Handle& operator=(Handle && other) noexcept
+	{
+		if (this != &other)
+		{
+			Reset(other.Detach());
+		}
+
+		return *this;
+	}
+
+	//Destructor
+	~Handle() noexcept
+	{
+		Close();
+	}
+
+	// Bool operator used to check the validety of a Handle. (is called by if(*this))
+	explicit operator bool() const noexcept
+	{
+		return m_value != Traits::Invalid();
+	}
+
+	// Get M_value
+	Type Get() const noexcept
+	{
+		return m_value;
+	}
+
+	//and a way to set it indirectly. we will assert that it is invalid. and return it's address.
+	//Where would this be used ? Well, many C style API's will return a handle as an out parameter 
+	//via a pointer to a handle, often a pointer to a pointer.This set method will return the address of the handle,
+	//so that such functions can set it, and the handle is immediately owned by the smart handle class.
+	//The assertion is also vitally important as it insurers that such overwriting of handles is not done.When a handle is already owned.
+	Type* Set() noexcept
+	{
+		ASSERT(!*this);
+		return &m_value;
+	}
+
+	//Detach ownerschip from a handle.
+	Type Detach() noexcept
+	{
+		Type value = m_value;
+		m_value = Traits::Invalid();
+		return value;
+	}
+
+	//A Way to safly attach handles even if a handle is already owned.
+	bool Reset(Type value = Traits::Invalid()) noexcept
+	{
+		if (m_value != value)
+		{
+			Close();
+			m_value = value;
+		}
+
+		return static_cast<bool>(*this);
+	}
+
+	//Swapping two handles
+	void Swap(Handle<Traits>& other) noexcept
+	{
+		Type temp = m_value;
+		m_value = other.m_value;
+		other.m_value = temp;
+	}
+};
+
+//Global swap
+template <typename Traits>
+void swap(Handle<Traits>& left, Handle<Traits> right) noexcept
+{
+	left.Swap(right);
+}
+
+//Equalaty, notEquality comparisons.
+template <typename Traits>
+bool operator==(Handle<Traits> const& left, Handle<Traits> const& right)
+{
+	return left.Get() == right.Get();
+}
+
+template <typename Traits>
+bool operator!=(Handle<Traits> const& left, Handle<Traits> const& right)
+{
+	return !(left == right);
+}
--- C++/SQLite/sqlite_1/sqlite_1/sqlite_1.cpp
+++ C++/SQLite/sqlite_1/sqlite_1/sqlite_1.cpp
@@ -1,37 +1,57 @@
 #include "sqlite3.h"
 #include <stdio.h>
+#include "Handle.h"
+
+#ifdef _DEBUG
+	#define VERIFY ASSERT
+	#define VERIFY_(result, expression) ASSERT(result == expression)
+#else
+	#define VERIFY(expression) (expression)
+	#define VERIFY_(result, expression) (expression)
+#endif
+
+struct ConnectionHandleTraits : HandleTraits<sqlite3*>
+{
+	static void Close(Type value) noexcept
+	{
+		VERIFY_(SQLITE_OK, sqlite3_close(value));
+	}
+};
+
+using ConnectionHandle = Handle<ConnectionHandleTraits>;
 
 int main()
 {
-	sqlite3* connection = nullptr;
+	//sqlite3* connection = nullptr;
+	ConnectionHandle connection;
 
-	int result = sqlite3_open(":memory:", &connection);
-
-	if (SQLITE_OK != result)
-	{
-		printf("%s\n", sqlite3_errmsg(connection));
-		sqlite3_close(connection);
-		return result;
-	}
-
-	sqlite3_stmt* query = nullptr;
-
-	result = sqlite3_prepare_v2(connection, "select 'Hello world'", -1, &query, nullptr);
+	int result = sqlite3_open(":memory:", connection.Set());
 
 	if (SQLITE_OK != result)
 	{
-		printf("%s\n", sqlite3_errmsg(connection));
-		sqlite3_close(connection);
+		printf("%s\n", sqlite3_errmsg(connection.Get()));
+		//sqlite3_close(connection);
 		return result;
 	}
 
-	while (SQLITE_ROW == sqlite3_step(query))
-	{
-		printf("%s\n", sqlite3_column_text(query, 0));
-	}
+	//sqlite3_stmt* query = nullptr;
 
-	sqlite3_finalize(query);
-	sqlite3_close(connection);
+	//result = sqlite3_prepare_v2(connection, "select 'Hello world'", -1, &query, nullptr);
+
+	//if (SQLITE_OK != result)
+	//{
+	//	printf("%s\n", sqlite3_errmsg(connection));
+	//	sqlite3_close(connection);
+	//	return result;
+	//}
+
+	//while (SQLITE_ROW == sqlite3_step(query))
+	//{
+	//	printf("%s\n", sqlite3_column_text(query, 0));
+	//}
+
+	//sqlite3_finalize(query);
+	//sqlite3_close(connection);
 
 }
 
